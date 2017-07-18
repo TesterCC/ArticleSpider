9fbd54c3882a5051824cffba078580f9bd5a45fe
LYX <testerlyx@foxmail.com>
2017-07-18 23:24:55 +0800

    20170718_01, build login zhihu post request.

--- ArticleSpider/utils/zhihu_login_requests.py
+++ ArticleSpider/utils/zhihu_login_requests.py
@@ -10,13 +10,24 @@ except:
     import http.cookiejar as cookielib   # python 3 need import http.cookiejar
 
 
-def zhihu_login(account, password):
-    # Login Zhihu
-    if re.match("^1\d{10}", account):   # 5-3 02:07
-        print("")
-# match moblie num,
+# match moblie num, ^1\d{10}
 # video match mobile num, here match e-mail [0-9a-zA-Z_]{0,19}@foxmail.com
 # match all email address, ^[0-9a-zA-Z_]{0,19}@[0-9a-zA-Z]{1,13}\.[com,cn,net]{1,3}$
+
+
+def zhihu_login(account, password):
+    # Login Zhihu
+    # if re.match("^1\d{10}", account):
+    if re.match("^[0-9a-zA-Z_]{0,19}@[0-9a-zA-Z]{1,13}\.[com,cn,net]{1,3}$", account):   # match e-mail
+        print("邮箱登陆")
+        post_url = "https://www.zhihu.com/login/phone_num"
+        post_data = {
+            "_xsrf": "",
+            "phone_num": account,
+            "password": password
+        }
+
+
 
 
 
