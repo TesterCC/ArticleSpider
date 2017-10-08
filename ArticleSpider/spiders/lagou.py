7794995be93501294fa7b9a775611523119d3dc6
LYX <testerlyx@foxmail.com>
2017-10-08 22:05:10 +0800

    20171008_03, learn CrawlSpider code.

--- ArticleSpider/spiders/lagou.py
+++ ArticleSpider/spiders/lagou.py
@@ -1,19 +1,27 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 
-class LagouSpider(CrawlSpider):
+class LagouSpider(CrawlSpider):        # 不能重写parse函数了,重载要用这个函数parse_start_url
     name = 'lagou'
     allowed_domains = ['www.lagou.com']
-    start_urls = ['http://www.lagou.com/']
+    start_urls = ['https://www.lagou.com/']     # most of websites use https
 
     rules = (
         Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     )
 
-    def parse_item(self, response):
+    def parse_start_url(self, response):
+        return []
+
+    def process_results(self, response, results):
+        return results
+
+    def parse_job(self, response):
+        # 解析拉勾网的职位
         i = {}
         #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
         #i['name'] = response.xpath('//div[@id="name"]').extract()
