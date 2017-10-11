6530081e9d3e4e84bcceae4554d07ef4fb82580c
LYX <testerlyx@foxmail.com>
2017-10-11 23:19:43 +0800

    20171011_01, start to code lagou spider.

--- ArticleSpider/spiders/lagou.py
+++ ArticleSpider/spiders/lagou.py
@@ -11,14 +11,17 @@ class LagouSpider(CrawlSpider):        # 不能重写parse函数了,重载要用
     start_urls = ['https://www.lagou.com/']     # most of websites use https
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=("zhaopin/.*",)), follow=True),        # 所有职位
+        Rule(LinkExtractor(allow=("gongsi/j\d+.html",)), follow=True),  # 公司页面的招聘信息
+        Rule(LinkExtractor(allow=r'jobs/\d+.html'), callback='parse_job', follow=True),
     )
 
-    def parse_start_url(self, response):
-        return []
-
-    def process_results(self, response, results):
-        return results
+# 不使用
+    # def parse_start_url(self, response):
+    #     return []
+    #
+    # def process_results(self, response, results):
+    #     return results
 
     def parse_job(self, response):
         # 解析拉勾网的职位
